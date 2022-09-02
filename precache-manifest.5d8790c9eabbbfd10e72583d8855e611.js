8655dc6365605aebcf576be806eb8162cd9e3356
Louis_Ekpenga <ekpenga9000@gmail.com>
2022-09-02 11:52:50 +0100

    Updates

---
+++ precache-manifest.5d8790c9eabbbfd10e72583d8855e611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9acdc0bfa150b99770a",
+    "url": "/expensetracker/static/css/main.a9a4733f.chunk.css"
+  },
+  {
+    "revision": "b9acdc0bfa150b99770a",
+    "url": "/expensetracker/static/js/main.b9acdc0b.chunk.js"
+  },
+  {
+    "revision": "d7ecf7b5f5da814c9053",
+    "url": "/expensetracker/static/css/1.0ac3d2e0.chunk.css"
+  },
+  {
+    "revision": "d7ecf7b5f5da814c9053",
+    "url": "/expensetracker/static/js/1.d7ecf7b5.chunk.js"
+  },
+  {
+    "revision": "afefb5745f3d13ed6309",
+    "url": "/expensetracker/static/js/runtime~main.afefb574.js"
+  },
+  {
+    "revision": "4fa9a7e80f5841568b12c1578a343dab",
+    "url": "/expensetracker/index.html"
+  }
+];
